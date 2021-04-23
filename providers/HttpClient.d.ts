57cb5a8165502e1a6a3e0d2a1f7c15aaef75231c
larryro <371767072@qq.com>
2021-04-23 16:36:33 +0800

    format code

--- providers/HttpClient.d.ts
+++
@@ -1,7 +0,0 @@
-import { THETA_METHOD_ENUM } from '../types/enum';
-export declare class HttpClient {
-    private url;
-    private nextId;
-    constructor(url: string);
-    send(method: THETA_METHOD_ENUM, params: any): Promise<any>;
-}
--- providers/HttpProvider.d.ts
+++
@@ -1,6 +0,0 @@
-import { HttpClient } from './HttpClient';
-export declare class ThetaHttpProvider {
-    httpClient: HttpClient;
-    constructor(url: string);
-    getBlockByHeight(blockHeight: number): Promise<any>;
-}
