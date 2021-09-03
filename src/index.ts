3fd93d0edfe2541fe1ef69fecf0848d970bf7a6d
larryro <371767072@qq.com>
2021-09-03 10:27:01 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { ThetaHttpProvider } from './providers/ThetaHttpProvider'
+export { CmcHttpProvider } from './providers/CmcHttpProvider'
 // export {THETA_TRANSACTION_TYPE_ENUM} from './types/enum'
 // module.exports.ThetaHttpProvider = ThetaHttpProvider
